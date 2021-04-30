411ed060fc135db399ad3c351d291773d6627996
kevin cuadros <kevindavidcuadros@gmail.com>
2021-04-29 22:40:41 -0500

    order funtions is complete

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -3,12 +3,61 @@ const Order = require('../models/Order')
 const OrderItem = require('../models/OrderItem')
 const User = require('../models/User')
 
+const createOrder = async (req = request, res = response) => {
+  try {
+    const { user, orderItems } = req.body
+
+    const validateUser = await User.findById(user)
+    if (!validateUser) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'El usuario no existe'
+      })
+    }
+
+    const orders = Promise.all(
+      orderItems.map(async (order) => {
+        let newsOrders = await new OrderItem(order).save()
+        return newsOrders._id
+      })
+    )
+    const ordersItemsResolved = await orders
+
+    const totalPrice = await Promise.all(
+      ordersItemsResolved.map(async (orderId) => {
+        const orderItem = await OrderItem.findById(orderId).populate('product', 'price')
+        const totalPrice = orderItem.product.price * orderItem.quantity
+        return totalPrice
+      })
+    )
+
+    const sumTotalPrice = totalPrice.reduce((acc, el) => acc + el, 0)
+
+    const order = new Order(req.body)
+    order.orderItems = ordersItemsResolved
+    order.totalPrice = sumTotalPrice
+    await order.save()
+
+    res.json({
+      ok: true,
+      order
+    })
+
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: 'Error al crear orden',
+      error
+    })
+  }
+}
+
 const getOrders = async (req, res = response) => {
   try {
-
     const orderList = await Order.find()
       .populate('user', 'name email phone, _id, isAdmin')
       .populate('orderItems')
+      .sort({ 'dateOrdered': -1 })
 
     if (!orderList) {
       return res.status(404).json({
@@ -31,30 +80,24 @@ const getOrders = async (req, res = response) => {
   }
 }
 
-const createOrder = async (req = request, res = response) => {
+const getOrderById = async (req = request, res = response) => {
   try {
-    const { user, orderItems } = req.body
+    const order = await Order.findById(req.params.id)
+      .populate('user', 'name email phone _id isAdmin')
+      .populate({
+        path: 'orderItems',
+        populate: {
+          path: 'product',
+          populate: 'category'
+        }
+      })
 
-    const validateUser = await User.findById(user)
-    if (!validateUser) {
+    if (!order) {
       return res.status(404).json({
         ok: false,
-        msg: 'El usuario no existe'
+        msg: 'No orden no esta registrada'
       })
     }
-
-    const orders = Promise.all(
-      orderItems.map(async (order) => {
-        let newsOrders = await new OrderItem(order).save()
-        return newsOrders._id
-      })
-    )
-
-    const ordersItemsResolved = await orders
-
-    const order = new Order(req.body)
-    order.orderItems = ordersItemsResolved
-    await order.save()
 
     res.json({
       ok: true,
@@ -64,7 +107,94 @@ const createOrder = async (req = request, res = response) => {
   } catch (error) {
     res.status(400).json({
       ok: false,
-      msg: 'Error al crear orden',
+      msg: 'Error al consultar ordenes',
+      error
+    })
+  }
+}
+
+const updateStatusOrder = async (req = request, res = response) => {
+  try {
+
+    const newOrder = await Order.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    if (!newOrder) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'El producto no existe'
+      })
+    }
+
+    res.json({
+      ok: true,
+      newOrder
+    })
+
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: 'Error actualizar estado del pedido',
+      error
+    })
+  }
+}
+
+const deleteOrder = async (req = request, res = response) => {
+  try {
+
+
+    const newOrder = await Order.findByIdAndDelete(req.params.id)
+
+    if (!newOrder) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'El producto ya fue eliminado'
+      })
+    }
+
+    const { orderItems } = newOrder
+
+    await Promise.all(orderItems.map(async (item) => {
+      await OrderItem.findByIdAndRemove(item)
+    }))
+
+    res.json({
+      ok: true,
+      newOrder
+    })
+
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: 'Error actualizar estado del pedido',
+      error
+    })
+  }
+}
+
+const getTotalSales = async (req, res = response) => {
+  try {
+    const totalSales = await Order.aggregate([
+      { $group: { _id: null, totalsales: { $sum: '$totalPrice' } } }
+    ])
+
+
+    if (!totalSales) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'Noy hay ventas registradas'
+      })
+    }
+
+    res.json({
+      ok: true,
+      totalSales: totalSales.pop().totalsales
+    })
+
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: 'Error al consultar ordenes',
       error
     })
   }
@@ -72,5 +202,9 @@ const createOrder = async (req = request, res = response) => {
 
 module.exports = {
   getOrders,
-  createOrder
+  createOrder,
+  deleteOrder,
+  getOrderById,
+  updateStatusOrder,
+  getTotalSales,
 }
--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const authValidate = require('../middleware/authValidate')
-const { getOrders, createOrder } = require('../controllers/orderController')
+const {
+  getOrders,
+  createOrder,
+  getOrderById,
+  updateStatusOrder,
+  deleteOrder,
+  getTotalSales } = require('../controllers/orderController')
 
 router.use(authValidate)
+
 router.get(`/`, getOrders)
+router.get(`/totalsales`, getTotalSales)
 router.post(`/`, createOrder)
+router.get(`/:id`, getOrderById)
+router.delete(`/:id`, deleteOrder)
+router.put(`/:id`, updateStatusOrder)
 
 
 module.exports = router
